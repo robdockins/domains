5f49136d07a2c8d52eea09fa795193eda1917a7e
robdockins <robdockins@fastmail.fm>
2014-01-23 17:25:05 -0800

    case analysis for finite types
    Ignore-this: 6ec1076b2a74f5832501a105a28a6dba
    
    darcs-hash:20140124012505-f399b-9aaaf1c969feffd7149055c497c534236cb8ac23

--- discrete.v
+++ discrete.v
@@ -124,6 +124,139 @@ Qed.
 
 Arguments disc_elem [Y] y.
 
+Section disc_cases.
+  Variable X:fintype.
+  Variables (A B:PLT).
+  Variable f : X -> (A → B).
+
+  Program Definition insert_index (x:X) : Preord.hom (A×B) ((A×disc X)×B) :=
+    Preord.Hom (A×B) ((A×disc X)×B) (fun ab => ((fst ab, x), snd ab)) _.
+  Next Obligation.
+    intros x [??] [??] [??]; simpl in *; auto.
+    split; simpl; auto. split; auto.
+  Qed.
+
+  Fixpoint mk_disc_cases_rel (ls:list X) : eset ((PLT.ord A×disc X)×B) :=
+    match ls with
+    | nil => ∅
+    | x::xs => union2 (image (insert_index x) (PLT.hom_rel (f x)))
+                      (mk_disc_cases_rel xs)
+    end.
+
+  Lemma mk_disc_cases_elem : forall ls x a b, (In x ls) ->
+    ((a,b) ∈ PLT.hom_rel (f x) <-> (a,x,b) ∈ mk_disc_cases_rel ls).
+  Proof.
+    induction ls; simpl; intros. elim H.
+    destruct H. subst a.
+    split; intros.
+    apply union2_elem. left.
+    apply image_axiom1'.
+    exists (a0,b); split; auto.
+    apply union2_elem in H.
+    destruct H. apply image_axiom2 in H.
+    destruct H as [[p q] [??]].
+    simpl in H0.
+    revert H. apply member_eq.
+    destruct H0 as [[??][??]]. simpl in *.
+    destruct H; destruct H1; simpl in *.
+    split; split; auto.
+    destruct (In_dec (fintype.fintype_dec X) x ls).
+    apply IHls; auto.
+    elim n. clear IHls. clear n.
+    induction ls; simpl in *.
+    apply empty_elem in H. auto.
+    apply union2_elem in H. destruct H.
+    apply image_axiom2 in H.
+    destruct H as [[p q] [??]].
+    simpl in H0.
+    destruct H0 as [[??][??]]. simpl in *.
+    destruct H0. simpl in *. hnf in H4. auto.
+    right; auto.
+    split; intros.
+    apply union2_elem. right. apply IHls; auto.
+    apply union2_elem in H0. destruct H0.
+    apply image_axiom2 in H0.
+    destruct H0 as [[p q] [??]]. simpl in H1.
+    destruct H1 as [[??][??]]. simpl in *.
+    destruct H1. simpl in *. destruct H3. simpl in *.
+    hnf in H5. subst a.
+    revert H0. apply member_eq.
+    split; split; auto.
+    apply IHls; auto.
+  Qed.
+
+  Program Definition disc_cases : PLT.prod A (disc X) → B :=
+    PLT.Hom false (PLT.prod A (disc X)) B
+       (mk_disc_cases_rel (fintype.fintype_list X)) _ _.
+  Next Obligation.
+    generalize (fintype.fintype_list X).
+    induction l; simpl; intros.
+    apply empty_elem in H1. elim H1.
+    apply union2_elem in H1. apply union2_elem.
+    destruct H1. left.
+    apply image_axiom2 in H1. apply image_axiom1'.
+    destruct H1 as [[p q] ?]. destruct H1.
+    simpl in H2.
+    destruct x; destruct x'. simpl.
+    destruct H. simpl in *. hnf in H3. subst c0.
+    exists (c1,y'). simpl. split.
+    destruct H2 as [[??][??]]. simpl in *.
+    destruct H2; destruct H4; simpl in *.
+    hnf in H6. subst c2. auto.
+    destruct H2 as [[??][??]]. simpl in *.
+    destruct H2; destruct H4; simpl in *.
+    revert H1. apply PLT.hom_order.
+    rewrite H4. auto.
+    rewrite H0; auto.   
+    right. eapply IHl; eauto.
+  Qed.
+  Next Obligation.
+    repeat intro.
+    destruct x as [a x].
+    destruct (PLT.hom_directed _ _ _ (f x) a M); auto.
+    red; simpl; intros. apply H in H0.
+    apply erel_image_elem in H0.
+    apply erel_image_elem.    
+    apply (mk_disc_cases_elem (fintype.fintype_list X) x a a0).
+    apply fintype.fintype_complete. auto.
+    destruct H0.    
+    apply erel_image_elem in H1.
+    exists x0. split; auto.
+    apply erel_image_elem.    
+    apply (mk_disc_cases_elem (fintype.fintype_list X) x a x0).
+    apply fintype.fintype_complete. auto.
+  Qed.
+
+  Lemma disc_cases_elem x h : disc_cases ∘ PLT.pair h (disc_elem x) ≈ f x ∘ h.
+  Proof.
+    split; intros a H. destruct a.
+    apply compose_hom_rel in H.
+    apply compose_hom_rel.
+    destruct H as [q [??]].
+    destruct q.
+    apply (mk_disc_cases_elem (fintype.fintype_list X)) in H0.
+    simpl in H.
+    rewrite (pair_rel_elem _ _ _ _ _ _ c c1 c2) in H. destruct H.
+    exists c1. split; auto.
+    apply single_axiom in H1.
+    destruct H1 as [[??][??]]. simpl in *.
+    hnf in H2. subst c2. auto.
+    apply fintype.fintype_complete.
+    destruct a.
+    apply compose_hom_rel in H.
+    apply compose_hom_rel.
+    destruct H as [q [??]].
+    exists (q,x). split.
+    apply pair_rel_elem. split; auto.
+    simpl. apply single_axiom.
+    destruct c. auto.
+    apply (mk_disc_cases_elem (fintype.fintype_list X)).
+    apply fintype.fintype_complete.
+    auto.   
+  Qed.
+
+End disc_cases.
+Arguments disc_cases [X A B] f.
 
 Program Definition finbool : fintype :=
   Fintype bool (true::false::nil) _ _.
